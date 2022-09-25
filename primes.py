12f1a784aaca08202a25ca8c08d54a4aef5a3eb0
Ivan <ivan.01.arabadzhiev@gmail.com>
2022-09-25 21:49:57 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -3,4 +3,9 @@
 
 def primes(number_of_primes):
     list = []
+    count = 2
+    while len(list) < number_of_primes:
+        for i in range(2, count // 2):
+            if count % i != 0:
+                list.append(count)
     return list
